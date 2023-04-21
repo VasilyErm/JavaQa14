e2a38df13bd281fcb790df295b78bb554e8f43cc
Vasily Ermolaev <vasily.erm@gmail.com>
2023-04-21 18:55:05 +0300

    Initional commit

--- src/test/java/ShopRepositoryTest.java
+++ src/test/java/ShopRepositoryTest.java
@@ -44,40 +44,22 @@ class ShopRepositoryTest {
         );
     }
 
-//    @Test
-//    public void testAlreadyExistProduct() {
-//
-//        ShopRepository shopRepository = new ShopRepository();
-//
-//        Product product1 = new Product(1, "Книга", 2000);
-//        Product product2 = new Product(2, "Одежда", 5000);
-//        Product product3 = new Product(3, "Часы", 9000);
-//
-//        shopRepository.add(product1);
-//        shopRepository.add(product2);
-//        shopRepository.add(product3);
-//
-//        Product[] actual = shopRepository.findAll();
-//        Product[] expected = {product1, product2, product3};
-//
-//        assertArrayEquals(actual, expected);
-//    }
-//
-//    @Test
-//    public void testAlreadyExistProductww() {
-//
-//        ShopRepository shopRepository = new ShopRepository();
-//
-//        Product product1 = new Product(1, "Книга", 2000);
-//        Product product2 = new Product(2, "Одежда", 9000);
-//        Product product3 = new Product(3, "Техника", 5000);
-//
-//        shopRepository.add(product1);
-//        shopRepository.add(product2);
-//        shopRepository.add(product3);
-//        shopRepository.add(product3);
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> shopRepository.remove(3)
-//        );
+    @Test
+    public void testAddProduct() {
+
+        ShopRepository shopRepository = new ShopRepository();
+
+        Product product1 = new Product(1, "Книга", 2000);
+        Product product2 = new Product(2, "Одежда", 5000);
+        Product product3 = new Product(3, "Часы", 9000);
+
+        shopRepository.add(product1);
+        shopRepository.add(product2);
+        shopRepository.add(product3);
+
+        Product[] actual = shopRepository.findAll();
+        Product[] expected = {product1, product2, product3};
+
+        assertArrayEquals(actual, expected);
+    }
 }
